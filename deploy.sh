cc51e35125c45dd177814486683470c16921c947
Olivier Gobilliard <olivier.gob@gmail.com>
2018-03-26 21:40:00 +0100

    Added Application Load Balancer and Target Group

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,9 @@ region="$(prop 'app.region')"
 appName="$(prop 'app.name')"
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cloudformation="file://cloudformation.json"
+subnetId="$(prop 'net.subnets.id')"
+sgId="$(prop 'net.sgs.id')"
+vpcId="$(prop 'net.vpc.id')"
 
 # Checking if the stack exists already
 error="$(aws cloudformation describe-stacks \
@@ -49,7 +52,10 @@ error="$(aws cloudformation $stack_action \
     --capabilities CAPABILITY_NAMED_IAM \
     --stack-name $appName \
     --template-body $cloudformation \
-    --region $region 2>&1 > /dev/null)"
+    --region $region \
+    --parameters ParameterKey=SubnetListParam,ParameterValue=$subnetId \
+      ParameterKey=SecurityGroupsListParam,ParameterValue=$sgId \
+      ParameterKey=VpcIdParam,ParameterValue=$vpcId 2>&1 > /dev/null)"
 
 # If aws cli cloudformation returned an error
 if [[ $? != 0 ]]; then
@@ -71,17 +77,36 @@ else
   echo "Cloudformation creation/update completed"
 fi
 
+targetGroup="$(aws cloudformation describe-stacks \
+                  --stack-name $appName \
+                  --region $region \
+                  --query 'Stacks[0].Outputs[0].OutputValue')"
 
+                  targetGroup="${targetGroup%\"}"
+                  targetGroup="${targetGroup#\"}"
+echo "AAAAAAAAAAAAAAA $targetGroup"
 
 # Generating ecs-params.yml
 echo "Generating ecs-params.yml"
-subnetId="$(prop 'net.subnet.id')"
-sgId="$(prop 'net.sg.id')"
-sed "s/NET_SUBNET_ID/$subnetId/g" ./ecs-params.template.yml > ./ecs-params.yml
+
+# TODO Dirty hack!! Find something better
+tempSub="${subnetId%\"}"
+tempSub="${tempSub#\"}"
+tempSub="$(echo $tempSub | cut -d',' -f1)"
+sed "s/NET_SUBNET_ID/$tempSub/g" ./ecs-params.template.yml > ./ecs-params.yml
 sed -i "s/NET_SG_ID/$sgId/g" ./ecs-params.yml
 
 # Deploying the cluster
 ecs-cli configure --cluster DockerEcsHelloWorldCluster --default-launch-type FARGATE --region us-east-1
-ecs-cli compose --project-name HelloWorldTutorial service up
+
+echo "stopping the service (if running)"
+ecs-cli compose --project-name HelloWorldTutorial service down 2>/dev/null
+
+echo starting the service
+ecs-cli compose --project-name HelloWorldTutorial service up \
+                --target-group-arn $targetGroup \
+                --container-name web \
+                --container-port 80
+
 
 exit
