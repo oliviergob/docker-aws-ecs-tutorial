0846e7b3cc5e4af69503763722051f53f3e55b08
Olivier Gobilliard <olivier.gob@gmail.com>
2018-04-10 22:25:18 +0100

    Progressed auto-scaling / need to automate

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask
 import os
 import socket
+import math
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 
+
+
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>"
+
+    # Forcing CPU usage
+    #for num in range(1,1000000):
+        #math.cos(math.sqrt((num*5678)/8));
+
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
